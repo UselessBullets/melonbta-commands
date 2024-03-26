60e3ba5badd8257036c2404e57d3e50602b064a8
UselessBullets <80850784+UselessBullets@users.noreply.github.com>
2024-03-26 02:34:38 -0500

    added sounds to kitten command

--- src/main/java/prophetsama/testing/commands/Kitten.java
+++ src/main/java/prophetsama/testing/commands/Kitten.java
@@ -4,9 +4,12 @@ import net.minecraft.core.net.command.Command;
 import net.minecraft.core.net.command.CommandHandler;
 import net.minecraft.core.net.command.CommandSender;
 
+import java.util.Random;
+
 public class Kitten extends Command {
 	private final static String COMMAND = "kitten";
 	private final static String NAME = "Kitten";
+	private final static Random random = new Random();
 
 	public Kitten() {
 		super(COMMAND);
@@ -14,11 +17,20 @@ public class Kitten extends Command {
 
 	@Override
 	public boolean execute(CommandHandler handler, CommandSender sender, String[] strings) {
-		if(Math.random() > .5)
-		sender.sendMessage("/ᐠ-ꞈ-ᐟ\\ ɴʏᴀ~");
-		else{
-		sender.sendMessage("/ᐠ - ˕ -マ ɴʏᴀᴀᴀᴀᴀ!");
+		if(random.nextDouble() > .5){
+			sender.sendMessage("/ᐠ-ꞈ-ᐟ\\ ɴʏᴀ~");
+			if (random.nextDouble() < .95){
+				sender.getPlayer().world.playSoundAtEntity(sender.getPlayer(), sender.getPlayer(), "mob.ghast.moan", 1f, 0.8f);
+			} else {
+				sender.getPlayer().world.playSoundAtEntity(sender.getPlayer(), sender.getPlayer(), "mob.wolf.bark", 1f, 1f);
+			}
+
 		}
+		else{
+			sender.sendMessage("/ᐠ - ˕ -マ ɴʏᴀᴀᴀᴀᴀ!");
+			sender.getPlayer().world.playSoundAtEntity(sender.getPlayer(), sender.getPlayer(), "mob.ghast.scream", 1f, 0.85f);
+		}
+
 		return true;
 	}
 
